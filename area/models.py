3c2ce2dd3e14e7c877b7dbca88d4935e2223b4f7
Alex <alexey@tigrio.ru>
2018-07-14 16:37:18 +0700

    add model area

--- area/models.py
+++ area/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class MicroArea(models.Model):
+    left_desc = 'левый нижний угол квадрата'
+    right_desc = 'правый нижний угол'
+    latitude = 'Широта'
+    longitude = 'Долгота'
+
+    latitude_left = models.FloatField(verbose_name=latitude,
+                                      help_text=left_desc)
+    longitude_left = models.FloatField(verbose_name=longitude,
+                                       help_text=left_desc)
+    latitude_right = models.FloatField(verbose_name=latitude,
+                                       help_text=right_desc)
+    longitude_right = models.FloatField(verbose_name=longitude,
+                                        help_text=right_desc)
+
+    class Meta:
+        verbose_name = 'Участок города'
+        verbose_name_plural = 'Участки города'
+
+    def __str__(self):
+        return 'Участок дороги № {}'.format(self.id)

27aa160a2b397bb68133b2435bd29e0119a2be7a
Oleg Marintsov <awwert@yandex.ru>
2018-07-14 20:40:41 +0700

    Add code verification logic

--- userdata/serializers.py
+++ userdata/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 import re
 from userdata.models import User
+import string
+import random
 
 
 class UserSerializer(serializers.Serializer):
@@ -21,8 +23,38 @@ class UserSerializer(serializers.Serializer):
 
         return attr
 
-    def create(self):
+    def create(self, session):
         self.is_valid(raise_exception=True)
         phone = self.data['phone']
-        user = User.objects.create(username=phone, phone=phone)
-        return user
+
+        code = ''.join(
+            random.choice(string.digits) for _ in range(self.CODE_LENGTH))
+
+        session['code_verification'] = code
+        session['phone'] = phone
+
+
+class CodeSerializer(serializers.Serializer):
+    default_error_messages = {
+        "incorrect_code": "Неверный код"
+    }
+
+    CODE_LENGTH = 6
+
+    code = serializers.CharField(max_length=CODE_LENGTH)
+
+    def validate_code(self, attr):
+
+        result = re.match(r"^\d{%d}$" % self.CODE_LENGTH, self.code)
+
+        if not result:
+            self.fail('incorrect_code')
+
+        if self.code != self.context['code']:
+            self.fail('incorrect_code')
+
+        return attr
+
+    def create(self, session):
+        phone = session['phone']
+        User.objects.create(username=phone, phone=phone)
--- userdata/views.py
+++ userdata/views.py
@@ -1,12 +1,23 @@
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.response import Response
-from userdata.serializers import UserSerializer
+from userdata.serializers import UserSerializer, CodeSerializer
 
 
 class RegisterUser(GenericViewSet):
     serializer_class = UserSerializer
 
     def create(self, request, *args, **kwargs):
-        serialaizer = self.get_serializer(data=request.data)
-        user = serialaizer.create()
-        return Response(data=user)
+        serializer = self.get_serializer(data=request.data)
+        serializer.create(request.session)
+        return Response(status=200)
+
+
+class CodeVerification(GenericViewSet):
+    serializer_class = CodeSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data,
+                                         context={
+                                             'code': request.session['code']})
+        serializer.create(request.session)
+        return Response(status=201)

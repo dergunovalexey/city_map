122fe48c67bc9ad6f953cdffae2dff97e0a66722
Alex <alexey@tigrio.ru>
2018-07-15 02:10:20 +0700

    fix compl views

--- complaint/views.py
+++ complaint/views.py
@@ -8,12 +8,12 @@ from complaint.serializers import ComplaintSerializer
 class ComplaintViews(ListModelMixin, CreateModelMixin, GenericViewSet):
     queryset = Complaint.objects.all()
     serializer_class = ComplaintSerializer
-    permission_classes = (IsAuthenticated, )
 
-    def get_queryset(self):
-        user = self.request.user
-        return Complaint.objects.filter(user=user
-                                        ).order_by('-created_at')
+    # def get_queryset(self):
+    #     user = self.request.user
+    #     ii
+    #     return Complaint.objects.filter(user=user
+    #                                     ).order_by('-created_at')
 
     def pre_save(self, obj):
         obj.photo = self.request.FILES.get('photo')

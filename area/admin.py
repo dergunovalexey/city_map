1e1d3ead59aa921dc4a59b7142a84f7c05cc5fba
Alex <alexey@tigrio.ru>
2018-07-14 20:45:12 +0700

    register microarea to admin

--- area/admin.py
+++ area/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from area.models import MicroArea
 
-# Register your models here.
+
+@admin.register(MicroArea)
+class MicroAreaAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('1я точка квадрата', {'fields': ('latitude_left', 'longitude_left')}),
+        ('2я точка квадрата',
+         {'fields': ('latitude_right', 'longitude_right')}),
+        ('состояние', {'fields': ('level',)}),
+    )

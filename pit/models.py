0c04afbd729c733cb4cdf9bac1b2d6cf37061c25
Alex <alexey@tigrio.ru>
2018-07-14 17:04:25 +0700

    add model pit

--- pit/models.py
+++ pit/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Pit(models.Model):
+    latitude = models.FloatField(verbose_name='Широта')
+    longitude = models.FloatField(verbose_name='Долгота')
+    number_of_jolting = models.PositiveIntegerField(
+        verbose_name='Количество толчков')
+    speed = models.FloatField(verbose_name='Скорость')
+
+    class Meta:
+        verbose_name = 'Сигнал неровности дороги'
+        verbose_name_plural = 'Сигналы неровности дороги'
+
+    def __str__(self):
+        return 'Сигнал неровности дороги №{}'.format(self.id)
